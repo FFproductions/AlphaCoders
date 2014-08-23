ab2ccf95b8ece02afa587e77ab004e712c4469e4
Blive77 <fabio.a.felgueiras@gmail.com>
2014-08-23 17:27:15 +0100

    Classes adicionadas
    Alunos, Grupo, Turma

---
+++ WPTrainerAssistant/WPTrainerAssistant/WPTrainerAssistant.Shared/Classes/CL_Grupo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WPTrainerAssistant.Classes
+{
+    class CL_Grupo
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Comentario { get; set; }
+        public List<CL_Turma> Lista_Turmas { get; set; }
+    }
+}
---
+++ WPTrainerAssistant/WPTrainerAssistant/WPTrainerAssistant.Shared/Classes/CL_Turma.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.Data.Xml.Dom;
+
+namespace WPTrainerAssistant.Classes
+{
+    class CL_Turma
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Comentario { get; set; }
+        public List<CL_Aluno> Lista_Alunos { get; set; }
+
+    }
+}
