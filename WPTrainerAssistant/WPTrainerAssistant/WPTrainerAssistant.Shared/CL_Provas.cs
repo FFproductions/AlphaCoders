924120faaf9dc0f3862197ff7af1b98e00378313
Skull63 <antonio.jorge.florindo@gmail.com>
2014-08-23 17:28:06 +0100

    novas classes
    novas classes

---
+++ WPTrainerAssistant/WPTrainerAssistant/WPTrainerAssistant.Shared/CL_Provas.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WPTrainerAssistant
+{
+    class CL_Provas
+    {
+        #region Attributes
+        private int _id;
+
+        public int Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        private string _comments;
+
+        public string Comments
+        {
+            get { return _comments; }
+            set { _comments = value; }
+        }
+
+        #endregion
+
+        #region Construtor(s)
+
+        public CL_Provas()
+        {
+            _id = 0;
+            _name = "";
+            _comments = "";
+        }
+
+        public CL_Provas(int id, string name, string comments)
+        {
+            _id = id;
+            _name = name;
+            _comments = comments;
+        }
+
+        #endregion
+
+        public static List<CL_Provas> ListaProvas()
+        {
+            List<CL_Provas> rList = new List<CL_Provas>();
+            //TODO - implement logic
+            return rList;
+        }
+    }
+}
---
+++ WPTrainerAssistant/WPTrainerAssistant/WPTrainerAssistant.Shared/CL_TabelaAvaliacao.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WPTrainerAssistant
+{
+    class CL_TabelaAvaliacao
+    {
+    }
+}
